6af610726ba61781c48af69da9f77ac1b25dd350
swj0 <sunanwenjun@163.com>
2018-01-01 22:17:27 +0800

    d

--- src/main/java/com/wen/jun/mq/common/domain/HeartbeatMessage.java
+++ src/main/java/com/wen/jun/mq/common/domain/HeartbeatMessage.java
@@ -3,6 +3,7 @@ package com.wen.jun.mq.common.domain;
 import com.wen.jun.mq.common.constant.MessageSource;
 import com.wen.jun.mq.common.constant.MessageType;
 
+//因为没有更多的信息要传递，所以暂时不使用，直接往channel写1即可
 public class HeartbeatMessage  implements java.io.Serializable{
 
 	/**
